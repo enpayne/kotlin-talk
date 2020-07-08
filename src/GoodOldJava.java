a38a86eecd6e66e0bb4774e50a0b525583dcef79
nick <nick.payne89@gmail.com>
2020-07-08 15:52:43 +0200

    a

---
+++ src/GoodOldJava.java
@@ -0,0 +1,25 @@
+
+
+
+
+public class GoodOldJava {
+
+    private String someString;
+    private int someInt;
+
+    public String getSomeString() {
+        return someString;
+    }
+
+    public void setSomeString(String someString) {
+        this.someString = someString;
+    }
+
+    public int getSomeInt() {
+        return someInt;
+    }
+
+    public void setSomeInt(int someInt) {
+        this.someInt = someInt;
+    }
+}
